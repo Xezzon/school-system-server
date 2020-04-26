afb415cd7b75fe73c0761f9872834f17ed6ab473
Xezzon <yzjdx4399@gmail.com>
2020-04-26 16:23:59 +0800

    bugfix, refactor: 修复测试环境下的读不到SecurityManager，适配maven test

--- school-passport/src/test/java/indi/xezzon/school/passport/service/AuthenticationServiceTest.java
+++ school-passport/src/test/java/indi/xezzon/school/passport/service/AuthenticationServiceTest.java
@@ -3,20 +3,33 @@ package indi.xezzon.school.passport.service;
 import cn.hutool.core.util.RandomUtil;
 import indi.xezzon.school.passport.model.Account;
 import org.apache.shiro.SecurityUtils;
-import org.junit.jupiter.api.Test;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.util.ThreadContext;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class AuthenticationServiceTest {
+public class AuthenticationServiceTest {
     @Autowired
     private AuthenticationService service;
     
+    // 解决测试环境下Shiro报错的问题，方法来源： https://www.oschina.net/question/4934_48690
+    @Resource
+    SecurityManager securityManager;
+    @Before
+    public void before() {
+        ThreadContext.bind(securityManager);
+    }
+    
     @Test
-    void register() {
+    public void register() {
         String text = RandomUtil.randomString(12);
         Account account = new Account();
         account.setUsername(text);
@@ -26,7 +39,7 @@ class AuthenticationServiceTest {
     }
     
     @Test
-    void login() {
+    public void login() {
         service.login("test", "test001", false);
         assert SecurityUtils.getSubject().isAuthenticated();
     }
