1f55526eb917158752176b59a6d8b9d1bd659024
Xezzon <yzjdx4399@gmail.com>
2020-05-08 12:05:27 +0800

    refactor(authentication): 修正account表的DDL
    由dbeaver生成。

--- school-passport/sql/account.sql
+++ school-passport/sql/account.sql
@@ -1,14 +1,14 @@
-create table account
+CREATE TABLE `account`
 (
-    id bigint unsigned auto_increment
-        primary key,
-    username varchar(20) not null comment '用户名',
-    cipher char(60) not null comment '密码',
-    status tinyint not null comment '账号状态',
-    created_time timestamp default current_timestamp() not null on update current_timestamp(),
-    updated_time timestamp default '0000-00-00 00:00:00' not null,
-    constraint account_username_uindex
-        unique (username)
-)
-    comment '账号';
-
+    `id`           bigint(20) unsigned                    NOT NULL AUTO_INCREMENT,
+    `username`     varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '用户名',
+    `cipher`       char(60) COLLATE utf8mb4_unicode_ci    NOT NULL COMMENT '密码',
+    `status`       tinyint(4)                             NOT NULL DEFAULT 0 COMMENT '账号状态',
+    `created_time` timestamp                              NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    `updated_time` timestamp                              NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `account_username_uindex` (`username`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 24
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='账号'
