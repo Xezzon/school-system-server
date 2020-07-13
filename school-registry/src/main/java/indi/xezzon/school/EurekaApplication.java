400c7aad253e042360a00949cbb4be7fe1cac9ae
Xezzon <yzjdx4399@gmail.com>
2020-07-13 19:02:10 +0800

    feature: 添加Eureka服务注册中心

---
+++ school-registry/src/main/java/indi/xezzon/school/EurekaApplication.java
@@ -0,0 +1,16 @@
+package indi.xezzon.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xezzon
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
