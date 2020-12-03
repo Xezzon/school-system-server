03e18c169141e32f949e71443ce2c1b486de6b16
Xezzon <yzjdx4399@gmail.com>
2020-12-03 21:31:37 +0800

    DAO层测试
    为CourseMapper新增测试类

---
+++ school-jwc/src/test/java/indi/xezzon/school/jwc/repository/CourseMapperTest.java
@@ -0,0 +1,15 @@
+package indi.xezzon.school.jwc.repository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class CourseMapperTest {
+    @Autowired
+    private CourseMapper mapper;
+}
