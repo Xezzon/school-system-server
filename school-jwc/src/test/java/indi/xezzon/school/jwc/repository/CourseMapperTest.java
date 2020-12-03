ae9914da5be1393b600f7f23f839a3cbff7b1d10
Xezzon <yzjdx4399@gmail.com>
2020-12-03 21:33:58 +0800

    测试用例1：查询所有课程
    实际开发中，开发和测试可以并行。即开发人员和测试人员约定好接口及期望后，双方分别提交代码和测试用例。

--- school-jwc/src/test/java/indi/xezzon/school/jwc/repository/CourseMapperTest.java
+++ school-jwc/src/test/java/indi/xezzon/school/jwc/repository/CourseMapperTest.java
@@ -1,10 +1,14 @@
 package indi.xezzon.school.jwc.repository;
 
+import indi.xezzon.school.common.model.Course;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,4 +16,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class CourseMapperTest {
     @Autowired
     private CourseMapper mapper;
+
+    @Test
+    public void selectList() {
+        List<Course> courses = mapper.selectList();
+        log.debug(courses.toString());
+    }
 }
