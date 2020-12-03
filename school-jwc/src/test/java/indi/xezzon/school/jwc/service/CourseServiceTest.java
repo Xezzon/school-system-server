58cccc8fdfad3cfad2ac7e25268ef93808357958
Xezzon <yzjdx4399@gmail.com>
2020-12-03 22:56:25 +0800

    CourseService.getAllCourses()测试用例1

--- school-jwc/src/test/java/indi/xezzon/school/jwc/service/CourseServiceTest.java
+++ school-jwc/src/test/java/indi/xezzon/school/jwc/service/CourseServiceTest.java
@@ -1,6 +1,7 @@
 package indi.xezzon.school.jwc.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,4 +13,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class CourseServiceTest {
     @Autowired
     private CourseService service;
+
+    @Test
+    public void getAllCourses() {
+        log.debug(service.getAllCourses().toString());
+    }
 }
