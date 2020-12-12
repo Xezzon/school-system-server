352589b4f68b2e28b03129091a7ea1b075c7382e
Xezzon <yzjdx4399@gmail.com>
2020-12-12 14:29:53 +0800

    refactor: 优化接口返回结果
    1. 对id进行hash化。
    2. 过滤字段created_time、updated_time。

--- school-common/src/main/java/indi/xezzon/school/common/model/Account.java
+++ school-common/src/main/java/indi/xezzon/school/common/model/Account.java
@@ -1,9 +1,23 @@
 package indi.xezzon.school.common.model;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import indi.xezzon.school.common.util.HashidsUtil;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -14,6 +28,8 @@ import java.time.LocalDateTime;
 @Setter
 @ToString
 public class Account implements Serializable {
+    @JsonSerialize(using = AccountIdSerializer.class)
+    @JsonDeserialize(using = AccountIdDeserializer.class)
     private Long id;
 
     /**
@@ -31,4 +47,28 @@ public class Account implements Serializable {
     private LocalDateTime updatedTime;
 
     private static final long serialVersionUID = 1L;
+}
+
+@PropertySource("classpath:config/hashids.properties")
+@Component
+class AccountIdSerializer extends JsonSerializer<Long> {
+    @Value("${account:tnuo}")
+    private String salt;
+
+    @Override
+    public void serialize(Long id, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeString(HashidsUtil.encode(id, salt));
+    }
+}
+
+@PropertySource("classpath:config/hashids.properties")
+@Component
+class AccountIdDeserializer extends JsonDeserializer<Long> {
+    @Value("${account:tnuo}")
+    private String salt;
+
+    @Override
+    public Long deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+        return HashidsUtil.decode(jsonParser.getText(), salt);
+    }
 }
