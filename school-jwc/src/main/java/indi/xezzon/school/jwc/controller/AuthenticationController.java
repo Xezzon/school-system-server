da26d488f7c93272d8b0b379560d38fc85ab23db
Xezzon <yzjdx4399@gmail.com>
2020-12-12 23:11:54 +0800

    feat: 定义API接口——注册

--- school-jwc/src/main/java/indi/xezzon/school/jwc/controller/AuthenticationController.java
+++ school-jwc/src/main/java/indi/xezzon/school/jwc/controller/AuthenticationController.java
@@ -1,5 +1,6 @@
 package indi.xezzon.school.jwc.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
+    @PostMapping("/register")
+    public void register(String username, String cipher) {
 
+    }
 }
