5b411350d5da78fb7566d563d93f78e9e400cf2f
Xezzon <yzjdx4399@gmail.com>
2020-12-13 00:29:27 +0800

    test: 测试Service层接口——注册

--- school-jwc/src/test/java/indi/xezzon/school/jwc/service/AuthenticationServiceTest.java
+++ school-jwc/src/test/java/indi/xezzon/school/jwc/service/AuthenticationServiceTest.java
@@ -1,7 +1,23 @@
 package indi.xezzon.school.jwc.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import cn.hutool.core.util.RandomUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
 public class AuthenticationServiceTest {
+    @Autowired
+    private AuthenticationService service;
 
+    @Test
+    public void register() {
+        String randomString = RandomUtil.randomString(8);
+        service.register(randomString, randomString);
+    }
 }
