43763709564cc555bda8127756addc812751f394
Xezzon <yzjdx4399@gmail.com>
2020-12-13 00:11:41 +0800

    feat: 添加Service层接口——认证

--- school-jwc/src/main/java/indi/xezzon/school/jwc/controller/AuthenticationController.java
+++ school-jwc/src/main/java/indi/xezzon/school/jwc/controller/AuthenticationController.java
@@ -1,5 +1,7 @@
 package indi.xezzon.school.jwc.controller;
 
+import indi.xezzon.school.jwc.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @Autowired
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
     @PostMapping("/register")
     public void register(String username, String cipher) {
 
---
+++ school-jwc/src/main/java/indi/xezzon/school/jwc/service/AuthenticationService.java
@@ -0,0 +1,7 @@
+package indi.xezzon.school.jwc.service;
+
+/**
+ * @author xezzon
+ */
+public interface AuthenticationService {
+}
