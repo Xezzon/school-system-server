0b1cc8c6ebf4bc19587683a2d2c14886dec73e8d
Xezzon <yzjdx4399@gmail.com>
2020-12-12 14:34:26 +0800

    test: 编写测试用例
    虽然很草率

--- school-jwc/src/test/java/indi/xezzon/school/jwc/service/CourseServiceTest.java
+++ school-jwc/src/test/java/indi/xezzon/school/jwc/service/CourseServiceTest.java
@@ -1,5 +1,7 @@
 package indi.xezzon.school.jwc.service;
 
+import indi.xezzon.school.common.model.Course;
+import indi.xezzon.school.common.model.PageResult;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +18,7 @@ public class CourseServiceTest {
 
     @Test
     public void getAllCourses() {
-        log.debug(service.getAllCourses().toString());
+        PageResult<Course> courses = service.getCoursesPaged(1, 20);
+        log.debug("{}", courses);
     }
 }
