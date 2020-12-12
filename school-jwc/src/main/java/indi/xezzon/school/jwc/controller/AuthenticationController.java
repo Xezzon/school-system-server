bb895f66dd8950c9399c1eb8c7f16b059f5d7cb2
Xezzon <yzjdx4399@gmail.com>
2020-12-12 23:02:38 +0800

    feat: 添加Controller类——认证

---
+++ school-jwc/src/main/java/indi/xezzon/school/jwc/controller/AuthenticationController.java
@@ -0,0 +1,13 @@
+package indi.xezzon.school.jwc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xezzon
+ */
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+}
