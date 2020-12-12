b72a7a940d32a81be3e5192d44f64437919e078c
Xezzon <yzjdx4399@gmail.com>
2020-12-12 23:29:44 +0800

    javadoc

--- school-jwc/src/main/java/indi/xezzon/school/jwc/repository/AccountMapper.java
+++ school-jwc/src/main/java/indi/xezzon/school/jwc/repository/AccountMapper.java
@@ -8,5 +8,11 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface AccountMapper {
+    /**
+     * 新增账号
+     *
+     * @param account 账号信息。其中用户名和密码为必填字段。
+     * @return 新增账号的id
+     */
     Long insert(Account account);
 }
