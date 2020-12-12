ffbf7cdc833e9e5ee3b065311b78bfe00d9b9120
Xezzon <yzjdx4399@gmail.com>
2020-12-12 14:37:39 +0800

    doc: 补充javadoc文档

--- school-jwc/src/main/java/indi/xezzon/school/jwc/service/CourseService.java
+++ school-jwc/src/main/java/indi/xezzon/school/jwc/service/CourseService.java
@@ -7,5 +7,12 @@ import indi.xezzon.school.common.model.PageResult;
  * @author xezzon
  */
 public interface CourseService {
+    /**
+     * 分页查询课程
+     *
+     * @param pageNum  页码
+     * @param pageSize 每页条数
+     * @return 课程列表
+     */
     PageResult<Course> getCoursesPaged(int pageNum, int pageSize);
 }
