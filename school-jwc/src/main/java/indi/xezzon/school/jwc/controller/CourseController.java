a428a72111b6bbdf95be97ec57f571865dad6b3d
Xezzon <yzjdx4399@gmail.com>
2020-12-12 12:46:25 +0800

    feat: 0.0.1接口

--- school-jwc/src/main/java/indi/xezzon/school/jwc/controller/CourseController.java
+++ school-jwc/src/main/java/indi/xezzon/school/jwc/controller/CourseController.java
@@ -1,13 +1,11 @@
 package indi.xezzon.school.jwc.controller;
 
 import indi.xezzon.school.common.model.Course;
+import indi.xezzon.school.common.model.PageResult;
 import indi.xezzon.school.jwc.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author xezzon
@@ -23,7 +21,19 @@ public class CourseController {
     }
 
     @GetMapping("/all")
-    public List<Course> courses() {
-        return courseService.getAllCourses();
+    public PageResult<Course> getCourses(@RequestParam("page_num") int pageNum, @RequestParam("page_size") int pageSize) {
+        return null;
+    }
+
+    @PostMapping("/elect")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void electCourse(long studentId, long courseId) {
+
+    }
+
+    @DeleteMapping("/elect")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void cancelElectCourse(long studentId, long courseId) {
+
     }
 }
