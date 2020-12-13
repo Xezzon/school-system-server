dc909f445a495dab81fd9af62c26f5874d679e3a
Xezzon <yzjdx4399@gmail.com>
2020-12-13 10:33:53 +0800

    feat: Shiro基础配置

--- school-jwc/src/main/java/indi/xezzon/school/jwc/config/ShiroConfig.java
+++ school-jwc/src/main/java/indi/xezzon/school/jwc/config/ShiroConfig.java
@@ -1,5 +1,19 @@
 package indi.xezzon.school.jwc.config;
 
+import cn.hutool.crypto.digest.BCrypt;
+import indi.xezzon.school.common.model.Account;
+import indi.xezzon.school.jwc.repository.AccountMapper;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.credential.CredentialsMatcher;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -7,5 +21,50 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class ShiroConfig {
+    @Bean
+    public DefaultWebSecurityManager securityManager() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(normalRealm());
+        return securityManager;
+    }
     
+    @Bean
+    public NormalRealm normalRealm() {
+        NormalRealm realm = new NormalRealm();
+        realm.setCredentialsMatcher(credentialsMatcher());
+        return realm;
+    }
+
+    @Bean
+    public CredentialsMatcher credentialsMatcher() {
+        return new BcryptCredentialMatcher();
+    }
+}
+
+class NormalRealm extends AuthorizingRealm {
+    @Autowired
+    private AccountMapper accountMapper;
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String username = (String) authenticationToken.getPrincipal();
+
+        Account account = accountMapper.selectByUsername(username);
+        String cipher = account.getCipher();
+        // 脱敏处理
+        account.setCipher(null);
+        return new SimpleAuthenticationInfo(account, cipher, this.getName());
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+}
+
+class BcryptCredentialMatcher implements CredentialsMatcher {
+    @Override
+    public boolean doCredentialsMatch(AuthenticationToken authenticationToken, AuthenticationInfo authenticationInfo) {
+        return BCrypt.checkpw(String.valueOf(authenticationToken.getCredentials()), String.valueOf(authenticationInfo.getCredentials()));
+    }
 }
