5552d53cd760d608a8c8bb8a85a3b83f3b84bfe0
Xezzon <yzjdx4399@gmail.com>
2020-12-13 10:09:35 +0800

    feat: 新增DAO层接口——依据用户名查询账号信息

--- school-jwc/src/main/java/indi/xezzon/school/jwc/repository/AccountMapper.java
+++ school-jwc/src/main/java/indi/xezzon/school/jwc/repository/AccountMapper.java
@@ -14,4 +14,12 @@ public interface AccountMapper {
      * @param account 账号信息。其中用户名和密码为必填字段。
      */
     void insert(Account account);
+
+    /**
+     * 通过用户名查询账号
+     *
+     * @param username 用户名
+     * @return 账号信息
+     */
+    Account selectByUsername(String username);
 }
