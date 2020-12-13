9ebc77c2445b2cd56345aee88d37e7160a28c8fe
Xezzon <yzjdx4399@gmail.com>
2020-12-13 12:18:19 +0800

    test: 测试Service层接口选课、退选课

--- school-jwc/src/test/java/indi/xezzon/school/jwc/service/CourseServiceTest.java
+++ school-jwc/src/test/java/indi/xezzon/school/jwc/service/CourseServiceTest.java
@@ -3,11 +3,16 @@ package indi.xezzon.school.jwc.service;
 import indi.xezzon.school.common.model.Course;
 import indi.xezzon.school.common.model.PageResult;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.util.ThreadContext;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,10 +20,31 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class CourseServiceTest {
     @Autowired
     private CourseService service;
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Resource
+    SecurityManager securityManager;
+
+    @Before
+    public void before() {
+        ThreadContext.bind(securityManager);
+        authenticationService.login("test_bcrypt", "test");
+    }
 
     @Test
     public void getAllCourses() {
         PageResult<Course> courses = service.getCoursesPaged(1, 20);
         log.debug("{}", courses);
+    }
+
+    @Test
+    public void electCourse() {
+        service.electCourse(1);
+    }
+
+    @Test
+    public void cancelElectCourse() {
+        service.cancelElectCourse(1);
     }
 }
