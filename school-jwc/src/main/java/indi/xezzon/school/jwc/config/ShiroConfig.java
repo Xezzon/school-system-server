57be727dd8d2bb75cb6ef8f3410ac8e9678c547d
Xezzon <yzjdx4399@gmail.com>
2020-12-13 09:47:47 +0800

    feat: 新增Shiro配置类

---
+++ school-jwc/src/main/java/indi/xezzon/school/jwc/config/ShiroConfig.java
@@ -0,0 +1,11 @@
+package indi.xezzon.school.jwc.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author xezzon
+ */
+@Configuration
+public class ShiroConfig {
+    
+}
