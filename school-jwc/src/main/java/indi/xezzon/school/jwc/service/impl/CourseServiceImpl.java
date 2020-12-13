24ecd8d00648019310223c82e873277cbbbe1022
Xezzon <yzjdx4399@gmail.com>
2020-12-13 12:01:29 +0800

    feat: 实现Service层接口——选课

--- school-jwc/src/main/java/indi/xezzon/school/jwc/service/impl/CourseServiceImpl.java
+++ school-jwc/src/main/java/indi/xezzon/school/jwc/service/impl/CourseServiceImpl.java
@@ -3,6 +3,8 @@ package indi.xezzon.school.jwc.service.impl;
 import indi.xezzon.school.common.model.Course;
 import indi.xezzon.school.common.model.PageResult;
 import indi.xezzon.school.jwc.repository.CourseMapper;
+import indi.xezzon.school.jwc.repository.CourseStudentRelMapper;
+import indi.xezzon.school.jwc.service.AuthenticationService;
 import indi.xezzon.school.jwc.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,10 +17,15 @@ import java.util.List;
 @Service
 public class CourseServiceImpl implements CourseService {
     private final CourseMapper courseMapper;
+    private final CourseStudentRelMapper courseStudentRelMapper;
+
+    private final AuthenticationService authenticationService;
 
     @Autowired
-    public CourseServiceImpl(CourseMapper courseMapper) {
+    public CourseServiceImpl(CourseMapper courseMapper, CourseStudentRelMapper courseStudentRelMapper, AuthenticationService authenticationService) {
         this.courseMapper = courseMapper;
+        this.courseStudentRelMapper = courseStudentRelMapper;
+        this.authenticationService = authenticationService;
     }
 
     @Override
@@ -32,7 +39,8 @@ public class CourseServiceImpl implements CourseService {
 
     @Override
     public void electCourse(long courseId) {
-
+        long studentId = authenticationService.getCurrentAccountId();
+        courseStudentRelMapper.insert(studentId, courseId);
     }
 
     @Override
