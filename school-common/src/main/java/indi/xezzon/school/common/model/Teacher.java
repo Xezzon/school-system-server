b825d3fa04c18e3d667982ebf143fa55bae81494
Xezzon <yzjdx4399@gmail.com>
2021-01-22 20:49:49 +0800

    refactor: 暂时删除ID哈希化相关代码

--- school-common/src/main/java/indi/xezzon/school/common/model/Teacher.java
+++ school-common/src/main/java/indi/xezzon/school/common/model/Teacher.java
@@ -1,23 +1,9 @@
 package indi.xezzon.school.common.model;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import indi.xezzon.school.common.util.HashidsUtil;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.io.Serializable;
 
 /**
@@ -27,8 +13,6 @@ import java.io.Serializable;
 @Setter
 @ToString
 public class Teacher implements Serializable {
-    @JsonSerialize(using = TeacherIdSerializer.class)
-    @JsonDeserialize(using = TeacherIdDeserializer.class)
     private Long id;
 
     /**
@@ -52,28 +36,4 @@ public class Teacher implements Serializable {
     private String readme;
 
     private static final long serialVersionUID = 1L;
-}
-
-@PropertySource("classpath:config/hashids.properties")
-@Component
-class TeacherIdSerializer extends JsonSerializer<Long> {
-    @Value("${teacher:rehc}")
-    private String salt;
-
-    @Override
-    public void serialize(Long id, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-        jsonGenerator.writeString(HashidsUtil.encode(id, salt));
-    }
-}
-
-@PropertySource("classpath:config/hashids.properties")
-@Component
-class TeacherIdDeserializer extends JsonDeserializer<Long> {
-    @Value("${teacher:rehc}")
-    private String salt;
-
-    @Override
-    public Long deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
-        return HashidsUtil.decode(jsonParser.getText(), salt);
-    }
 }
