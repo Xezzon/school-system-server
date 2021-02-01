7d3e15d3ca518dbb8be583b90bc2fa7a95be7956
Xezzon <yzjdx4399@gmail.com>
2021-02-01 21:48:18 +0800

    refactor: 根据不同的阶段有不同的选课策略

--- school-jwc/src/main/java/indi/xezzon/school/jwc/service/impl/CourseServiceImpl.java
+++ school-jwc/src/main/java/indi/xezzon/school/jwc/service/impl/CourseServiceImpl.java
@@ -1,16 +1,21 @@
 package indi.xezzon.school.jwc.service.impl;
 
+import cn.hutool.core.map.MapBuilder;
+import cn.hutool.core.util.EnumUtil;
 import indi.xezzon.school.common.model.Course;
 import indi.xezzon.school.common.model.PageResult;
+import indi.xezzon.school.jwc.constant.enums.ElectCourseStatusEnum;
 import indi.xezzon.school.jwc.repository.CourseMapper;
-import indi.xezzon.school.jwc.repository.CourseStudentRelMapper;
 import indi.xezzon.school.jwc.service.CourseService;
 import indi.xezzon.school.jwc.service.FeignAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author xezzon
@@ -18,16 +23,20 @@ import java.util.List;
 @Service
 public class CourseServiceImpl implements CourseService {
     private final CourseMapper courseMapper;
-    private final CourseStudentRelMapper courseStudentRelMapper;
     private final FeignAuthService authService;
     @Autowired
     private HttpSession session;
+    private final Map<ElectCourseStatusEnum, ElectCourseHandler> electCourseHandlers;
+    @Autowired
+    private final RedisTemplate<String, Serializable> redisTemplate;
 
     @Autowired
-    public CourseServiceImpl(CourseMapper courseMapper, CourseStudentRelMapper courseStudentRelMapper, FeignAuthService authService) {
+    public CourseServiceImpl(CourseMapper courseMapper, FeignAuthService authService, RedisTemplate<String, Serializable> redisTemplate) {
         this.courseMapper = courseMapper;
-        this.courseStudentRelMapper = courseStudentRelMapper;
         this.authService = authService;
+        this.redisTemplate = redisTemplate;
+        this.electCourseHandlers = MapBuilder.<ElectCourseStatusEnum, ElectCourseHandler>create()
+                .map();
     }
 
     @Override
@@ -42,12 +51,37 @@ public class CourseServiceImpl implements CourseService {
     @Override
     public void electCourse(long courseId) {
         long studentId = authService.getCurrentAccountId(session.getId());
-        courseStudentRelMapper.insert(studentId, courseId);
+        // 从Redis获取当前选课状态并转换为Enum
+        ElectCourseHandler electCourseHandler = electCourseHandlers.get(EnumUtil.getEnumMap(ElectCourseStatusEnum.class).get((String) redisTemplate.opsForValue().get("context:status:elect-course")));
+        electCourseHandler.electCourse(courseId, studentId);
     }
 
     @Override
     public void cancelElectCourse(long courseId) {
         long studentId = authService.getCurrentAccountId(session.getId());
-        courseStudentRelMapper.delete(studentId, courseId);
+        ElectCourseHandler electCourseHandler = electCourseHandlers.get(EnumUtil.getEnumMap(ElectCourseStatusEnum.class).get((String) redisTemplate.opsForValue().get("context:status:elect-course")));
+        electCourseHandler.cancelElectCourse(courseId, studentId);
     }
+}
+
+/**
+ * 策略模式
+ * 不同时段对选课和退选课使用不同的策略
+ */
+interface ElectCourseHandler {
+    /**
+     * 选课
+     *
+     * @param courseId  课程ID
+     * @param studentId 学生ID
+     */
+    void electCourse(long courseId, long studentId);
+
+    /**
+     * 退选课
+     *
+     * @param courseId  课程ID
+     * @param studentId 学生ID
+     */
+    void cancelElectCourse(long courseId, long studentId);
 }
