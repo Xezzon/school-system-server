23319130d5781fcc80df71943821ddc2c5b3464b
xezzon <yzjdx4399@gmail.com>
2021-06-28 17:31:16 +0800

    feature: 切面记录WebAPI请求日志

---
+++ school-common/src/main/java/indi/xezzon/school/common/manager/RequestAspect.java
@@ -0,0 +1,50 @@
+package indi.xezzon.school.common.manager;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Objects;
+
+/**
+ * @author xezzon
+ */
+@Component
+@Aspect
+@Slf4j
+public class RequestAspect {
+    @Pointcut("execution(public * indi.xezzon.school.*.controller.*.*(..))")
+    public void requestAspect() {
+    }
+
+    @Around("requestAspect()")
+    public Object doAround(ProceedingJoinPoint pjp) {
+        LocalDateTime beginDateTime = LocalDateTime.now();
+        long beginTimestamp = beginDateTime.toInstant(ZoneOffset.ofHours(8)).toEpochMilli();
+        String uri = null;
+        String method = null;
+        String ip = null;
+        Object result = null;
+        try {
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            HttpServletRequest request = Objects.requireNonNull(attributes).getRequest();
+            uri = request.getRequestURI();
+            method = request.getMethod();
+            ip = request.getRemoteAddr();
+            result = pjp.proceed();
+        } catch (Throwable throwable) {
+            log.error("{} 于北京时间{} {} {}失败 {}", ip, beginDateTime, method, uri, throwable.getMessage());
+        }
+        long endTimestamp = System.currentTimeMillis();
+        log.debug("ACCESS-: {}于北京时间{}请求{},用时{}ms", ip, beginDateTime, uri, endTimestamp - beginTimestamp);
+        return result;
+    }
+}
